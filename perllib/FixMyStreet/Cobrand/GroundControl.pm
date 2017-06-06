24e00c5c22b38782410ad9ab47034b669b559ad5
Dave Arter <davea@mysociety.org>
2017-06-06 15:20:41 +0100

    [Ground Control] Cover all EUR areas

--- perllib/FixMyStreet/Cobrand/GroundControl.pm
+++ perllib/FixMyStreet/Cobrand/GroundControl.pm
@@ -12,6 +12,12 @@ sub council_area { 'Ground Control' }
 sub council_name { 'Ground Control' }
 sub council_url { 'groundcontrol' }
 
+# We want nice high-level areas, as the site can cover all of the UK.
+sub area_types { [ 'EUR' ] }
+sub area_types_children { [] }
+# Assume this cobrand covers all areas
+sub area_check { 1 }
+
 sub example_places { ( 'CM12 0EQ', "Foys Walk, South Green" ) }
 
 1;
